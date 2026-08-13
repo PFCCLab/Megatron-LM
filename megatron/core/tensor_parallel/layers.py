2ab854fc93660a46a64429ead8bc87fb2d61a156
zhanghonggeng <zhanghonggeng@baidu.com>
2026-08-13 14:19:29 +0800

    fix embedding deterministic mode check

--- megatron/core/tensor_parallel/layers.py
+++ megatron/core/tensor_parallel/layers.py
@@ -298,16 +298,11 @@ class VocabParallelEmbedding(torch.nn.Module):
         else:
             masked_input = input_
         # Get the embeddings.
-        from megatron.core.transformer.module import _use_accuracy_compatible
-
-        if _use_accuracy_compatible():
+        if self.deterministic_mode:
             output_parallel = self.weight[masked_input]
         else:
-            if self.deterministic_mode:
-                output_parallel = self.weight[masked_input]
-            else:
-                # F.embedding currently has a non-deterministic backward function
-                output_parallel = F.embedding(masked_input, self.weight)
+            # F.embedding currently has a non-deterministic backward function
+            output_parallel = F.embedding(masked_input, self.weight)
         # Mask the output embedding.
         if self.tp_group.size() > 1:
             output_parallel[input_mask, :] = 0.0
